c8341a1d2f99924df00a872e700a53421d7e69b4
saracavalcante <designersaracavalcante@gmail.com>
2022-01-18 11:11:09 -0300

    Adiciona mensagem de erro

--- desafio-ioasys-books/app/src/main/java/br/com/ioasys/ioasysbooks/MainActivity.kt
+++ desafio-ioasys-books/app/src/main/java/br/com/ioasys/ioasysbooks/MainActivity.kt
@@ -2,10 +2,21 @@ package br.com.ioasys.ioasysbooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatTextView
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var errorMessage = findViewById<AppCompatTextView>(R.id.errorMessage)
+        val enterButton = findViewById<MaterialButton>(R.id.btnEnterButton)
+
+        enterButton.setOnClickListener {
+            errorMessage.visibility = View.VISIBLE
+        }
     }
 }
